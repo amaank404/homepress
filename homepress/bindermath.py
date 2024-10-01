ec2e1a05b1949b77e06aaf2fff80c37d80e140bd
Amaan <71920621+amaank404@users.noreply.github.com>
2024-10-01 23:31:08 +0530

    Remove single sided math, use the new type hinting system

--- homepress/bindermath.py
+++ homepress/bindermath.py
@@ -2,11 +2,10 @@
 Contains all the page order and numbering algorithms for binding
 """
 
-from typing import Tuple, List, Union
 
-PageIndexType = int
+type PageIndexType = int
 
-def doubleside_singlestack_midpage(numpages) -> List[Tuple[Tuple[PageIndexType, PageIndexType], Tuple[PageIndexType, PageIndexType]]]:
+def doubleside_singlestack_midpage(numpages) -> list[tuple[tuple[PageIndexType, PageIndexType], tuple[PageIndexType, PageIndexType]]]:
     out = []
     if numpages % 4 != 0:  # Ceil it with 4 as whole steps. Make it all a multiple of 4
         numpages += 4-(numpages%4)
@@ -16,16 +15,4 @@ def doubleside_singlestack_midpage(numpages) -> List[Tuple[Tuple[PageIndexType, 
         out.append(((lastpage, startpage), (startpage+1, lastpage-1)))
         lastpage -= 2
         startpage += 2
-    return out
-
-def singleside_singlestack_midpage(numpages) -> List[Tuple[PageIndexType, PageIndexType]]:
-    out = []
-    if numpages % 2 != 0:  # Ceil it with 2 as whole steps. Make it all a multiple of 2
-        numpages += 1
-    lastpage = numpages-1
-    startpage = 0
-    while startpage < lastpage:
-        out.append((startpage, lastpage))
-        lastpage -= 1
-        startpage += 1
     return out
