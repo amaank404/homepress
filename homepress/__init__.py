68b58eee4f369226c3901b5a74851fdf4fd00d87
Amaan <71920621+amaank404@users.noreply.github.com>
2024-10-21 18:30:12 +0530

    update version 2.0.1

--- homepress/__init__.py
+++ homepress/__init__.py
@@ -2,5 +2,5 @@ from .press import Press
 from .progress import Progress
 
 __all__ = ["Press", "Progress"]
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __homepage__ = "https://github.com/amaank404/homepress"
