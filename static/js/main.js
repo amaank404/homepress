eb2d79d8776bbc48a9ced5c0a1a076273d4b721e
Amaan <71920621+amaank404@users.noreply.github.com>
2024-10-29 12:22:06 +0530

    fixed glow animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,3 @@
-$(".banner-blur").removeClass("opacity-0").addClass("animation-glow");
+$(".banner-blur").removeClass("opacity-0");
+
+setTimeout(() => {$(".banner-blur").addClass("animation-glow");}, 4000);
