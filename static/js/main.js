a3f803abbe12a4ab3136906c928bc531bdc589fb
Amaan <71920621+amaank404@users.noreply.github.com>
2024-10-29 20:39:54 +0530

    animate title

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,7 @@
 $(".banner-blur").removeClass("opacity-0");
 
 setTimeout(() => {$(".banner-blur").addClass("animation-glow");}, 4000);
+
+$(window).on('scroll', () => {
+    $('body').css("--scrolly", window.scrollY);
+})
